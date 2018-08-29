3c1bd8fd7c0900b2932fdac29f3ca06c01fe7a23
Bappel <bappel@Bappels-MacBook-Pro.local>
2018-08-29 21:13:40 +1000

    UI finish, ready to test

---
+++ DoseWise/DoseWise/ViewControllers/EditNomeneeViewController.swift
@@ -0,0 +1,12 @@
+import Foundation
+import UIKit
+
+class EditNomeneeViewController : UIViewController{
+    
+    override func viewDidLoad(){
+        
+    }
+    @IBAction func dismissNomineeView(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
