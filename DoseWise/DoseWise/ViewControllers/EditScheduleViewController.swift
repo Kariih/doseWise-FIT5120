8be58610fa71afcd447d9589bb655896d04b6659
Bappel <bappel@Bappels-MacBook-Pro.local>
2018-10-01 19:04:27 +1000

    added UI for multiple schedule

--- DoseWise/DoseWise/ViewControllers/EditScheduleViewController.swift
+++ DoseWise/DoseWise/ViewControllers/EditScheduleViewController.swift
@@ -4,29 +4,30 @@ import iOSDropDown
 
 class EditScheduleViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource{
     
-    @IBOutlet weak var daysLbl: UILabel!
-    @IBOutlet weak var dosageLbl: UILabel!
     @IBOutlet weak var timesLbl: UILabel!
-    @IBOutlet weak var medicineNameTxt: DropDown!
     @IBOutlet weak var setTimesLbl: UILabel!
+    
+    @IBOutlet weak var medicineNameTxt1: DropDown!
+    @IBOutlet weak var medicineNameTxt2: DropDown!
+    @IBOutlet weak var medicineNameTxt3: DropDown!
+    @IBOutlet weak var medicineNameTxt4: DropDown!
+    
+    @IBOutlet weak var amountLbl1: UITextField!
+    @IBOutlet weak var amountLbl2: UITextField!
+    @IBOutlet weak var amountLbl3: UITextField!
+    @IBOutlet weak var amountLbl4: UITextField!
+    
     
     @IBOutlet weak var deleteBtn: UIButton!
     @IBOutlet weak var timeLbl1: UIButton!
-    @IBOutlet weak var timeLbl2: UIButton!
-    @IBOutlet weak var timeLbl3: UIButton!
-    @IBOutlet weak var timeLbl4: UIButton!
-    @IBOutlet weak var timeLbl5: UIButton!
     let dbSchedule = CRUDDrugSchedule()
-    
-    @IBOutlet weak var stepperOne: UIStepper!
-    @IBOutlet weak var stepperTwo: UIStepper!
     @IBOutlet weak var stepperThree: UIStepper!
     
     var clickedTimedBtn : UIButton!
-    var numberOfTimesDay : Int!
+    var numberOfPillsOnSchedule : Int!
     
     @IBOutlet weak var timePickerView: UIPickerView!
-    var timeLabels : [UIButton] = []
+    var pillLabels : [(DropDown, UITextField)] = []
     
     override
     func viewDidLoad() {
@@ -34,16 +35,16 @@ class EditScheduleViewController: UIViewController, UIPickerViewDelegate, UIPick
         timePickerView.dataSource = self
         timePickerView.delegate = self
         
-        timeLabels.append(timeLbl1)
-        timeLabels.append(timeLbl2)
-        timeLabels.append(timeLbl3)
-        timeLabels.append(timeLbl4)
-        timeLabels.append(timeLbl5)
+        pillLabels.append((medicineNameTxt1, amountLbl1))
+        pillLabels.append((medicineNameTxt2, amountLbl2))
+        pillLabels.append((medicineNameTxt3, amountLbl3))
+        pillLabels.append((medicineNameTxt4, amountLbl4))
       
         GetMeds.Shared.MED_DATA.sort()
-        medicineNameTxt.optionArray = GetMeds.Shared.MED_DATA // load searchbar
-       
-      
+        medicineNameTxt1.optionArray = GetMeds.Shared.MED_DATA
+        medicineNameTxt2.optionArray = GetMeds.Shared.MED_DATA
+        medicineNameTxt3.optionArray = GetMeds.Shared.MED_DATA
+        medicineNameTxt4.optionArray = GetMeds.Shared.MED_DATA// load searchbar
         timePickerView.backgroundColor = UIColor.white
         
         if !Const.currentSchedule.timings.isEmpty{
@@ -55,28 +56,22 @@ class EditScheduleViewController: UIViewController, UIPickerViewDelegate, UIPick
     override
     func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        if medicineNameTxt.optionArray[0] == ""{
-        
+        if medicineNameTxt1.optionArray[0] == ""{
             EmptyMedlistpopup()
-         
         }
-      
     }
     
     private func setDataForEdit(){
         let drug = Const.currentSchedule
-        daysLbl.text = String(drug.no_of_days)
-        stepperOne.value = Double(drug.no_of_days)
-        dosageLbl.text = String(drug.no_of_pills_per_dose)
-        stepperTwo.value = Double(drug.no_of_pills_per_dose)
         timesLbl.text = String(drug.no_of_times_per_day)
         stepperThree.value = Double(drug.no_of_times_per_day)
-        numberOfTimesDay = drug.no_of_times_per_day
-        medicineNameTxt.text = drug.name
+        numberOfPillsOnSchedule = drug.no_of_times_per_day
+        medicineNameTxt1.text = drug.name
         
         for i in 0...drug.no_of_times_per_day-1{
-            timeLabels[i].setTitle(drug.timings[i], for: .normal)
-            timeLabels[i].isHidden = false
+         //   pillLabels[i].setTitle(drug.timings[i], for: .normal)
+            pillLabels[i].1.isHidden = false
+            pillLabels[i].0.isHidden = false
         }
     }
     
@@ -86,19 +81,11 @@ class EditScheduleViewController: UIViewController, UIPickerViewDelegate, UIPick
         Const.currentSchedule = DrugSchedule()
         dismissView()
     }
-    
-    @IBAction func daysStepperAction(_ sender: UIStepper) {
-        daysLbl.text = String(Int(sender.value))
-    }
-    
-    @IBAction func doseageStepperAction(_ sender: UIStepper) {
-        dosageLbl.text = String(Int(sender.value))
-    }
-    
-    @IBAction func timeStepperAction(_ sender: UIStepper) {
+
+    @IBAction func pillAmountStepperAction(_ sender: UIStepper) {
         let previousValue = Int(timesLbl.text!)
         let currentValue = Int(sender.value)
-        numberOfTimesDay = currentValue
+        numberOfPillsOnSchedule = currentValue
         timesLbl.text = String(currentValue)
         
         if currentValue > 0{
@@ -110,36 +97,18 @@ class EditScheduleViewController: UIViewController, UIPickerViewDelegate, UIPick
         
         if previousValue! > currentValue{
             for i in currentValue...previousValue!-1{
-                timeLabels[i].isHidden = true
+                pillLabels[i].0.isHidden = true
+                pillLabels[i].1.isHidden = true
             }
         }else{
             for i in previousValue!...currentValue-1{
-                timeLabels[i].isHidden = false
+                pillLabels[i].0.isHidden = false
+                pillLabels[i].1.isHidden = false
             }
         }
     }
     
     @IBAction func timeBtnOneClick(_ sender: UIButton) {
-        clickedTimedBtn = sender
-        timePickerView.isHidden = false
-    }
-    
-    @IBAction func timeBtnTwoClick(_ sender: UIButton) {
-        clickedTimedBtn = sender
-        timePickerView.isHidden = false
-    }
-    
-    @IBAction func timeBtnThreeClick(_ sender: UIButton) {
-        clickedTimedBtn = sender
-        timePickerView.isHidden = false
-    }
-    
-    @IBAction func timeBtnFourClick(_ sender: UIButton) {
-        clickedTimedBtn = sender
-        timePickerView.isHidden = false
-    }
-    
-    @IBAction func timeBtnFiveClick(_ sender: UIButton) {
         clickedTimedBtn = sender
         timePickerView.isHidden = false
     }
@@ -158,33 +127,19 @@ class EditScheduleViewController: UIViewController, UIPickerViewDelegate, UIPick
     
     @IBAction func saveBtnClick(_ sender: Any) {
         var timings: [String] = []
-        if numberOfTimesDay != nil{
+        if numberOfPillsOnSchedule != nil{
             let notificationManager = NotificationReminderManager()
             notificationManager.addReminder(time: "1pm")
-            for i in 0...numberOfTimesDay-1{
-                timings.append(timeLabels[i].title(for: .normal)!)
+            for i in 0...numberOfPillsOnSchedule-1{
+             //   timings.append(pillLabels[i].title(for: .normal)!)
             }
             //Add check for nil values here
-            let schedule = DrugSchedule(id: 0, name: medicineNameTxt.text!, no_of_days: Int(daysLbl.text!)!, no_of_times_per_day: Int(timesLbl.text!)!, no_of_pills_per_dose: Int(dosageLbl.text!)!, timings: timings, type_of_pill: "opioid")
+       //     let schedule = DrugSchedule(id: 0, name: medicineNameTxt1.text!, no_of_days: Int(daysLbl.text!)!, no_of_times_per_day: Int(timesLbl.text!)!, no_of_pills_per_dose: Int(dosageLbl.text!)!, timings: timings, type_of_pill: "opioid")
             dbSchedule.deleteAllDrugSchedule()
-            dbSchedule.addDrugSchedule(DrugSchedule: schedule)
+         //   dbSchedule.addDrugSchedule(DrugSchedule: schedule)
         }
         dismissView()
     }
-//    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-//        return 1
-//    }
-//
-//    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-//        return Const.TIMES_A_DAY.count
-//    }
-//    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-//        clickedTimedBtn.setTitle(Const.TIMES_A_DAY[row], for: .normal)
-//        timePickerView.isHidden = true
-//    }
-//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-//        return Const.TIMES_A_DAY[row]
-//    }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return Const.TIMES_A_DAY.count
@@ -194,15 +149,6 @@ class EditScheduleViewController: UIViewController, UIPickerViewDelegate, UIPick
         return Const.TIMES_A_DAY[component].count
     }
     
-//    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-//        let hour = Const.TIMES_A_DAY[0][pickerView.selectedRow(inComponent: 0)]
-//        let minute = Const.TIMES_A_DAY[1][pickerView.selectedRow(inComponent: 1)]
-//        let amOrPm = Const.TIMES_A_DAY[2][pickerView.selectedRow(inComponent: 2)]
-//        let theTiming:String = hour + ":" + minute + " " + amOrPm
-//        clickedTimedBtn.setTitle(theTiming, for: .normal)
-//        timePickerView.isHidden = true
-//    }
-
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         let hour = Const.TIMES_A_DAY[0][pickerView.selectedRow(inComponent: 0)]
         let minute = Const.TIMES_A_DAY[1][pickerView.selectedRow(inComponent: 1)]
